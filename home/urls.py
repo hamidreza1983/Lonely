12245df2ad6b74bf33477d0a784c5fe2a8b8eb4b
hamid reza <hamidrezacoder1983@gmail.com>
2024-02-25 14:37:24 +0330

    start training

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from home.views import home, portfolio_details
+
+
+
+urlpatterns = [
+    path('', home, name='home' ),
+    path('portfolio-details/<int:id>', portfolio_details, name='portfolio-dretails' )
+]
