a6c76a9cf8b2c08a276780954f14c26a3cbf216c
Ayda <aydaghasemi66@gmail.com>
2024-02-28 01:27:53 +0330

    ,

--- portfolio/api/V1/views.py
+++ portfolio/api/V1/views.py
@@ -1,19 +1,6 @@
-
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializer import *
 from ...models import *
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView , ListAPIView, ListCreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework import viewsets
-from .permission import IsAdminOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from .paginator import CustomePaginate
 
 class PortfolioDetailView(GenericAPIView, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin):   
     #queryset = Course.objects.filter(status=True)
---
+++ portfolio/views/PortfolioListView.py
@@ -0,0 +1,23 @@
+from ..models import Portfolio
+from ..views import PortfolioDetailView
+from django.views.generic import ListView
+
+class PortfolioListView(ListView):
+    """This class represents a ListView for displaying portfolios.
+    It sets the template name to 'home/index.html', specifies the context object name as 'portfolios',
+    and sets pagination to display 9 portfolios per page."""
+
+    template_name = 'home/index.html'
+    context_object_name = 'portfolios'
+    paginate_by = 9
+
+    def get_queryset(self):
+        """"Returns the queryset of portfolios based on the category specified in the URL kwargs."""
+        if self.kwargs.get('cat'):
+            return Portfolio.objects.filter(category__name=self.kwargs.get('cat'))
+        else:
+            return Portfolio.objects.filter(status=True) 
+    def post(self, request, *args, **kwargs):
+        """Handles POST requests by delegating the handling to PortfolioDetailView."""
+        post_detail = PortfolioDetailView()
+        return post_detail.post(request,*args,**kwargs)
