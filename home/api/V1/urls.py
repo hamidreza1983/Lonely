43e467c7f796418d4aebf5b12b1ba0539ccafb1b
fatemedavoodi <bahare.khazan98@gmail.com>
2024-02-28 15:24:12 +0330

    urls

--- home/api/V1/urls.py
+++ home/api/V1/urls.py
@@ -11,9 +11,9 @@ app_name = 'api-v1'
 urlpatterns = [
     path("home/",CategoryListView.as_view(),name='category'),
     path("home/",ServicesListView.as_view(),name='services'),
-    path("home/",ServiceDetailView.as_view(),name='service_detail'),
+    path("home/<int:pk>",ServiceDetailView.as_view(),name='service_detail'),
     path("home/",ResumeListView.as_view(),name='resume'),
-    path("home/",ResumeDetailView.as_view(),name='resume'),
+    path("home/<int:pk>",ResumeDetailView.as_view(),name='resume'),
     path("home/",SkillsListView.as_view(),name='skills'),
 
 
