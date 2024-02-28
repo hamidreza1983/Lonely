0592895c10e66a434a7f58cd5d26cb4f12a25cd4
neg2004ah <neginahmadi533@gmail.com>
2024-02-28 17:59:36 +0330

    debugging

--- home/api/V1/serializer/serviceserializer.py
+++ home/api/V1/serializer/serviceserializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from home.models import Services
 class ServiceSerializer(serializers.ModelSerializer):
-    '''this class is for service serializer in home
-'''
-
+    '''
+    this class is for service serializer in home
+    '''
     class Meta:
         model = Services
         fields = ["title", "content", "category","image", 'status']
